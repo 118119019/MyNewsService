920d89ec624dc96dfb8a74bc6d1b889934b81a9f
118119019 <xie118119019@126.com>
2015-07-12 17:25:52 +0800

    suojian

---
+++ WebApplication2/Global.asax.cs
@@ -0,0 +1,33 @@
+﻿using ServiceHelp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.SessionState;
+
+namespace WebApplication2
+{
+    public class Global : System.Web.HttpApplication
+    {
+        private MySchedulerService myscheduler = MySchedulerService.GetInstance();
+        void Application_Start(object sender, EventArgs e)
+        {
+            // 在应用程序启动时运行的代码
+            myscheduler.Run();
+
+        }
+
+        void Application_End(object sender, EventArgs e)
+        {
+            //  在应用程序关闭时运行的代码
+            myscheduler.ShutDown();
+        }
+
+        void Application_Error(object sender, EventArgs e)
+        {
+            // 在出现未处理的错误时运行的代码
+
+        }
+    }
+}
---
+++ WebApplication2/Handler1.ashx.cs
@@ -0,0 +1,61 @@
+﻿using CommonService.Serilizer;
+using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+
+namespace WebApplication2
+{
+    /// <summary>
+    /// Handler1 的摘要说明
+    /// </summary>
+    public class Handler1 : IHttpHandler
+    {
+
+        public void ProcessRequest(HttpContext context)
+        {
+            context.Response.ContentType = "text/plain";
+            var Response = context.Response;
+            if (context.Request["url"] != null)
+            {
+                string url = context.Request["url"];
+                try
+                {
+                    ArticleJson articleJson = new ArticleJson();
+                    string content = CommonUtility.HttpUtility.Get(url, Encoding.UTF8);
+                    HtmlDocument doc = new HtmlDocument();
+                    doc.LoadHtml(content);
+                    var div = doc.DocumentNode.SelectSingleNode("//div[@id='content']");
+                    articleJson.Content = div.InnerText;
+                    div = doc.DocumentNode.SelectSingleNode("//div[@class='title article_info']");
+                    articleJson.Title = div.SelectSingleNode("h1").InnerText;
+                    div = doc.DocumentNode.SelectSingleNode("//div[@id='extra']");
+                    articleJson.date = div.SelectSingleNode("span[@class='date']").InnerText.Replace("阅读全文", "");
+                    var str = SerilizeService<ArticleJson>.CreateSerilizer(Serilize_Type.Json).Serilize(articleJson);
+                    Response.Output.Write(str);
+                }
+                catch (Exception ex)
+                {
+                     
+                } 
+            } 
+            Response.End();
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+    public class ArticleJson
+    {
+        public string Title { get; set; }
+        public string date { get; set; }
+        public string Content { get; set; }
+    }
+}
