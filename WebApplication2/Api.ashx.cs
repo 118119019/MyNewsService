3bb1bf54cf29cbc6ab15c974ff9e2243118f10a8
118119019 <xie118119019@126.com>
2015-08-04 18:51:19 +0800

    201508041851

---
+++ WebApplication2/Api.ashx.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication2
+{
+    /// <summary>
+    /// Api 的摘要说明
+    /// </summary>
+    public class Api : IHttpHandler
+    {
+
+        public void ProcessRequest(HttpContext context)
+        {
+            var request = context.Request;
+            var action = request["action"] ?? "";
+            if (action == "")
+            {
+                context.Response.ContentType = "text/plain";
+                context.Response.Write("Hello World");
+            }
+            else
+            {
+                //方法 getList getById
+            }
+
+
+            //http://msdn.microsoft.com/en-us/library/system.web.httpresponse.binarywrite(v=vs.110).aspx
+
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
