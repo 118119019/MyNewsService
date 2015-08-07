cc9c6ce4ab10ba8ac2ff6714d2505b343074f566
118119019 <xie118119019@126.com>
2015-08-07 18:29:07 +0800

    163

---
+++ News.Model/163/Topic.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace News.Model._163
+{
+    public class Topic
+    {
+        /// <summary>
+        /// "原创"
+        /// </summary>
+        public string tname { get; set; }
+        /// <summary>
+        /// "zhenhua"
+        /// </summary>
+        public string ename { get; set; }
+        /// <summary>
+        /// T1370583240249
+        /// </summary>
+        public string tid { get; set; }
+    }
+
+}
